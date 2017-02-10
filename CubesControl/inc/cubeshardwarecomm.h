25aa257a941039ae6ccf07b71b2b7520dcf45b20
Theodor Stana <stana@kth.se>
2017-02-10 14:05:27 +0100

    CubesControl: Adding LGPL headers to CubesHardwareComm.cpp and .h

--- CubesControl/inc/cubeshardwarecomm.h
+++ CubesControl/inc/cubeshardwarecomm.h
@@ -1,3 +1,41 @@
+/*
+ *==============================================================================
+ * KTH Royal Institute of Technology Stockholm
+ * CUBES Hardware Level
+ *==============================================================================
+ *
+ * author: Theodor Stana (theodor.stana@gmail.com)
+ *
+ * date of creation: 2017-02-08
+ *
+ * version: 1.0
+ *
+ * description:
+ *
+ * dependencies:
+ *
+ * references:
+ *
+ *==============================================================================
+ * GNU LESSER GENERAL PUBLIC LICENSE
+ *==============================================================================
+ * This source file is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation; either version 2.1 of the License, or (at your
+ * option) any later version. This source is distributed in the hope that it
+ * will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details. You should have
+ * received a copy of the GNU Lesser General Public License along with this
+ * source; if not, download it from http://www.gnu.org/licenses/lgpl-2.1.html
+ *==============================================================================
+ * last changes:
+ *    2017-02-08   theodor.stana@gmail.com     File created
+ *==============================================================================
+ * TODO: -
+ *==============================================================================
+ */
+
 #ifndef CUBESHARDWARECOMM_H
 #define CUBESHARDWARECOMM_H
 
@@ -14,7 +52,10 @@ public:
     explicit CubesHardwareComm(QSerialPort *port, QLabel *lblMessages, QObject *parent = 0);
     ~CubesHardwareComm();
 
+    void write(const QByteArray &writeData);
+
 private slots:
+    void handleReadyRead();
     void handleTimeout();
     void handleError(QSerialPort::SerialPortError error);
 
--- CubesControl/src/cubeshardwarecomm.cpp
+++ CubesControl/src/cubeshardwarecomm.cpp
@@ -1,4 +1,42 @@
-#include "CubesHardwareComm.h"
+/*
+ *==============================================================================
+ * KTH Royal Institute of Technology Stockholm
+ * CUBES Hardware Level
+ *==============================================================================
+ *
+ * author: Theodor Stana (theodor.stana@gmail.com)
+ *
+ * date of creation: 2017-02-08
+ *
+ * version: 1.0
+ *
+ * description:
+ *
+ * dependencies:
+ *
+ * references:
+ *
+ *==============================================================================
+ * GNU LESSER GENERAL PUBLIC LICENSE
+ *==============================================================================
+ * This source file is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation; either version 2.1 of the License, or (at your
+ * option) any later version. This source is distributed in the hope that it
+ * will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details. You should have
+ * received a copy of the GNU Lesser General Public License along with this
+ * source; if not, download it from http://www.gnu.org/licenses/lgpl-2.1.html
+ *==============================================================================
+ * last changes:
+ *    2017-02-08   theodor.stana@gmail.com     File created
+ *==============================================================================
+ * TODO: -
+ *==============================================================================
+ */
+
+#include "cubeshardwarecomm.h"
 
 #include <QtSerialPort/QSerialPort>
 #include <QLabel>
@@ -9,6 +47,7 @@ CubesHardwareComm::CubesHardwareComm(QSerialPort *port, QLabel *lblMessages, QOb
     , m_lblMessages(lblMessages)
 {
     m_timer.setSingleShot(true);
+    QObject::connect( m_port, &QSerialPort::readyRead, this, &CubesHardwareComm::handleReadyRead);
     QObject::connect( m_port, static_cast<void (QSerialPort::*)(QSerialPort::SerialPortError)>(&QSerialPort::error),
                       this, &CubesHardwareComm::handleError );
     QObject::connect( &m_timer, &QTimer::timeout,
@@ -19,12 +58,23 @@ CubesHardwareComm::~CubesHardwareComm()
 {
 }
 
+inline void CubesHardwareComm::write(const QByteArray &writeData)
+{
+    m_port->write(writeData);
+}
+
+void CubesHardwareComm::handleReadyRead()
+{
+    m_lblMessages->setText(QString::fromUtf8(m_port->readAll()));
+}
+
 void CubesHardwareComm::handleError(QSerialPort::SerialPortError error)
 {
-    m_lblMessages->setText("Serial port error occured: " + QString::number(error) + "\n");
+    m_lblMessages->setText("Serial port error occured: " + QString::number(error));
 }
 
 void CubesHardwareComm::handleTimeout()
 {
-    m_lblMessages->setText("Operation timed out for port " + m_port->portName() + "; error" + m_port->errorString() + "\n");
+    m_lblMessages->setText("Operation timed out for port " + m_port->portName() +
+                           "; error" + m_port->errorString());
 }
