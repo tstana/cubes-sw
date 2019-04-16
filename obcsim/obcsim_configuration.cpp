705b901f001a01c9740d73d9a12650226a6b5dcf
Theodor Stana <stana@kth.se>
2019-04-16 15:48:25 +0200

    OBC-sim implementation for sending single ASIC config
    The simulator now goes through the following process:
    
    1. Read ASIC config files with the names configured in
    "obcsim_configuration.cpp";
    2. Write config to ASIC;
    3. Stop.
    
    This commit also changes the length of the filename strings to
    13, rather than the previous 12, which was wrong. We need 13
    characters in the filename:
    
    - 8 chars for file name
    - 1 char for dot between file name and extension
    - 3 chars for extension
    - 1 char for C's null terminator ("\0")
    
    [The 8+1+3 combination for the filename stems from Arduino's SD
    card library file naming convetion: 8.3 files are used, that is,
    8 chars (max.) for file names and three for extensions.]

--- obcsim/obcsim_configuration.cpp
+++ obcsim/obcsim_configuration.cpp
@@ -10,8 +10,8 @@ static unsigned char data_SEND_PARAMS[] = {0x50, 0x00, 0x10, 0x22};
 static unsigned char data_SEND_CITI_CONF[143];
 static unsigned char data_SEND_PROB_CONF[32];
 static unsigned char data_seqinit2[] = {0xAA, 0x10, 0x50, 0x30};
-static char conf[12] = "asic.cfg";
-static char prob[12] = "probe.cfg";
+static char conf[13] = "asic.cfg";
+static char prob[13] = "probec00.cfg";
 
 static char cfg_sent = 0;
 
@@ -32,22 +32,20 @@ void sequence_init(msp_link_t *lnk)
 
 void sequence_loop(msp_link_t *lnk)
 {
-  if (!cfg_sent) {
-  	Serial.println("-------- Invoking SEND_CITI_CONF --------");
-  	invoke_send(lnk, MSP_OP_SEND_CITI_CONF, data_SEND_CITI_CONF, sizeof(data_SEND_CITI_CONF), NONE);
-  	Serial.println("-----------------------------------------\n");
-
-    delay(5000);
-
-    Serial.println("-------- Invoking SEND_PROB_CONF --------");
-    invoke_send(lnk, MSP_OP_SEND_PROB_CONF, data_SEND_PROB_CONF, sizeof(data_SEND_PROB_CONF), NONE);
-    Serial.println("-----------------------------------------\n");
-
-    cfg_sent = 1;
-  }
+	Serial.println("-------- Invoking SEND_CITI_CONF --------");
+	invoke_send(lnk, MSP_OP_SEND_CITI_CONF, data_SEND_CITI_CONF, sizeof(data_SEND_CITI_CONF), BYTES);
+	Serial.println("-----------------------------------------\n");
 
   delay(5000);
 
-  Serial.println("OBC sim still alive!");
+  Serial.println("-------- Invoking SEND_PROB_CONF --------");
+  invoke_send(lnk, MSP_OP_SEND_PROB_CONF, data_SEND_PROB_CONF, sizeof(data_SEND_PROB_CONF), BYTES);
+  Serial.println("-----------------------------------------\n");
 
+  Serial.println("-----------------------------------------");
+  Serial.println("End of OBC simulator operation.");
+  Serial.println("-----------------------------------------\n");
+  
+  for (;;)
+    ;
 }
