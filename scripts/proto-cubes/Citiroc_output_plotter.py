0cc1eab613b096eff17361e0a8a9d4bd7c101d54
Marcus Persson <zethiane@gmail.com>
2019-06-25 15:29:28 +0200

    Add script for plotting Citiroc data

---
+++ scripts/proto-cubes/Citiroc_output_plotter.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+import time
+import datetime
+import subprocess
+import sys
+import binascii
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+from struct import unpack
+
+filename = input("Enter file name: ")
+f = open(filename, 'rb')
+filecontent = bytearray(f.read())
+start = filecontent.find("Unix time: ")+11 #Offsetting "Unix time: "
+unixtime = int(filecontent[start:start+10])
+binno = unpack('>h',filecontent[start+266:start+268])
+i = 0
+datavalues = list(range(0, 12287, 1))
+for x in range(start+269, len(filecontent)-146, 2):
+    try:
+        datavalues[i] = unpack('=H', filecontent[x:x+2])[0]
+    except:
+        print("Value at location %i in the file has an issue" %x)
+    i=i+1
+
+
+f.close()
+fig = plt.figure()
+ax = plt.subplot(111)
+ax.plot(datavalues)
+plt.ylabel('Energy levels')
+plt.xlabel('ADC channels')
+plt.title('DAQ retrieved')
+plt.annotate("Unix time: %i" %unixtime, xy=(9000, 67500))
+plt.annotate("Bins: %i" %binno, xy=(500, 67500))
+plt.grid(True)
+fig.savefig('DAQ.png')
+plt.close(fig)
+print("DONE")
