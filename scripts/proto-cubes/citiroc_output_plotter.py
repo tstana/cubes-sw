1f3a8dd76177bc3ea965233c64defaa6630510c2
Theodor Stana <stana@kth.se>
2019-07-24 15:07:21 +0200

    Make Citiroc output plotter plot actual data
    The previous version was inducing a staircase plot through the way the data
    values arrays were being initialized. With the changes implemented here,
    actual data with their expected offsets in the file are plotted.

--- scripts/proto-cubes/citiroc_output_plotter.py
+++ scripts/proto-cubes/citiroc_output_plotter.py
@@ -29,49 +29,55 @@ for x in range(start+269, len(filecontent)-146, 2):
         print("Value at location %i in the file has an issue" %x)
     i=i+1
 
-datavalues = list(range(0, 2047, 1))
-datavalues1 = list(range(2048, 4095, 1))
-datavalues2 = list(range(4096, 6143, 1))
-datavalues3 = list(range(6144, 8191, 1))
-datavalues4 = list(range(8192, 10239, 1))
-datavalues5 = list(range(10240, 12287, 1))
-
-
 f.close()
-filename = filename[:-3]
-filename = filename + 'png'
-fig = plt.figure(figsize=(6, 9))
+
+plt.figure(figsize=(6, 9))
 plt.suptitle("Bins: %i   Unix time: %i" %(binno[0],unixtime))
 
+ax0 = plt.subplot(321)
+ax0.plot(datavalues[0:2047])
+ax0.title.set_text('Ch 0 HG')
+plt.ylabel('Counts')
+plt.xlabel('ADC channels')
+plt.grid(True)
 
-ax = plt.subplot(321)
-ax.plot(datavalues)
-ax.title.set_text('Ch 0 HG')
-plt.grid(True)
 ax1 = plt.subplot(323)
-ax1.plot(datavalues1)
+ax1.plot(datavalues[2048:4095])
 ax1.title.set_text('Ch 16 HG')
+plt.ylabel('Counts')
+plt.xlabel('ADC channels')
 plt.grid(True)
+
 ax2 = plt.subplot(325)
-ax2.plot(datavalues2)
+ax2.plot(datavalues[4096:6143])
 ax2.title.set_text('Ch 31 HG')
 plt.ylabel('Counts')
 plt.xlabel('ADC channels')
 plt.grid(True)
+
 ax3 = plt.subplot(322)
-ax3.plot(datavalues3)
+ax3.plot(datavalues[6144:8191])
 ax3.title.set_text('Ch 0 LG')
+plt.ylabel('Counts')
+plt.xlabel('ADC channels')
 plt.grid(True)
+
 ax4 = plt.subplot(324)
-ax4.plot(datavalues4)
+ax4.plot(datavalues[8192:10239])
 ax4.title.set_text('Ch 16 LG')
+plt.ylabel('Counts')
+plt.xlabel('ADC channels')
 plt.grid(True)
+
 ax5 = plt.subplot(326)
-ax5.plot(datavalues5)
+ax5.plot(datavalues[10240:12287])
 ax5.title.set_text('Ch 31 LG')
 plt.ylabel('Counts')
 plt.xlabel('ADC channels')
 plt.grid(True)
 
-fig = plt.tight_layout()
+plt.tight_layout()
+
+print("Waiting for the user to close the plot...")
+
 plt.show()
