23981045c16aa625d72da0906eae4a2895a969e7
Theodor Stana <stana@kth.se>
2022-10-22 19:54:37 +0200

    Redesign convert_config_file.py
    The file is now used to generate default configuration for the Citiroc
    ASIC, to be loaded by the MSS firmware on CUBES initialization.

--- scripts/convert_config_file.py
+++ scripts/convert_config_file.py
@@ -1,41 +1,85 @@
 #!/usr/bin/python3
+from datetime import date
 import math
 import binascii
+import sys
+import os
+import argparse
+
 
 def Generator(s, n):
 	for start in range(0, len(s)-1, n):
 		yield s[start:start+n]
 
 def invert_bits(s):
-	print(s)
-	t = s[::-1]
-	print(t)
-	print("-")
-	return t
+	return s[::-1]
 
-inputname = input("Enter input file name: ")
+GCC_HEADER = """
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the “Software”), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+"""
 
-while (1):
-	sel = int(input("What will you be configuring? (1: ASIC, 2: Probes) "))
-	if (sel == 1):
-		outputname = "asic.cfg"
-		break;
-	elif (sel == 2):
-		outputname = "probe.cfg"
-		break;
-	else:
-		print("ERROR: Please select (1: ASIC) or (2: Probes)\n");
+def write_defconfig(inputname):
+	f = open(inputname, 'r')
+	conf = f.readline()
+	f.close()
 
-f = open(inputname, 'r') #Name of input file
-g = open(outputname, 'wb') #Name of output file 
-fline = f.readline()
-i = 0;
-for y in Generator(fline, 8):
-	print("addr = 0x{0:x}".format(i))
-	i += 1
-	y=(int(invert_bits(y), 2))
-	g.write(y.to_bytes(1, 'little'))
-f.close()
-g.close()
-print("Wrote successfully to " + outputname);
-#
+	OUTPUTNAME = "citiroc_defconfig.h"
+
+	g = open(OUTPUTNAME, 'w')
+
+	g.write("/*\n")
+	g.write(" * Default Citiroc Configuration\n")
+	g.write(" *\n")
+	g.write(" * Generated from " + inputname + " on " + str(date.today()) +
+		" using\n" +
+		" * " + os.path.basename(__file__) + "\n")
+	g.write(" *")
+	g.write(GCC_HEADER)
+	g.write(" */\n")
+	g.write('\n')
+
+	g.write("const uint8_t CITIROC_DEFCONFIG = {")
+
+	i = 0;
+	for y in Generator(conf, 8):
+		if i%8 == 0:
+			g.write('\n')
+			g.write('\t')
+		else:
+			g.write(' ')
+		y = (int(invert_bits(y), 2))
+		g.write("0x{0:02x}".format(y) + ',')
+		i += 1
+
+	g.write(" 0x00")  # CONF_ID field
+	g.write("\n}")
+	g.write('\n')
+
+	g.close()
+
+	print("Wrote successfully to " + OUTPUTNAME);
+
+if __name__ == "__main__":
+	parser = argparse.ArgumentParser(description="Convert Citiroc .conf file " +
+		"expoted by CitirocUI to default configuration string to be applied " +
+		" by MSS on CUBES initialization.")
+	parser.add_argument("filename", help=".conf file exported by CitirocUI")
+	args = parser.parse_args()
+
+	write_defconfig(args.filename)
