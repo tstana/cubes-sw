7701bfb6abbaed53b125da170b8ad8cb52a8679f
Theodor Stana <theodor.stana@gmail.com>
2024-10-21 12:27:49 +0200

    Clarify value_conv.py interface
    - Tell user they should input voltage in volts (not just a number)
    - Print also decimal value for volt/temp to hex conversion, so that it
      is usable with the MIST OBC and MCS.

--- scripts/value_conv.py
+++ scripts/value_conv.py
@@ -12,21 +12,23 @@ print()
 types = input("Your selection: ")
 
 if types == '1':
-	number = input("Write the number in hex: ")
+	number = input("Write the voltage in hex: ")
 	i = int(number, 16)
 	output = i * 1.812e-3
 if types == '2':
-	number = float(input("Write the number in decimal: "))
+	number = float(input("Write the voltage in volts: "))
 	i = int(number / 1.812e-3)
 	output = hex(i)
+	output += f" ({int(output, 16)})"
 if types == '3':
 	number = int(input("Write the temperature in hex: "), 16)
 	i = (number*(1.907e-5)-1.035)/(-5.5e-3)
 	output = i
 if types == '4':
-	number = float(input("Write the temperature in degrees: "))
+	number = float(input("Write the temperature in degrees C: "))
 	i = int(((number*(-5.5e-3))+1.035)/1.907e-5)
 	output = hex(i)
+	output += f" ({int(output, 16)})"
 
 print(output)
 
